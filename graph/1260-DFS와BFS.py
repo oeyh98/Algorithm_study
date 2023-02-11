bebfdc67e8416848f3db494b2df5f7d34667f950
oeyh98 <skjrjr123@naver.com>
2023-02-11 23:30:07 +0900

    1

--- graph/1260-DFS와BFS.py
+++ graph/1260-DFS와BFS.py
@@ -5,6 +5,8 @@ n, m, v = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(n+1)]
 dfs_visited = [0] * (n+1)
 bfs_visited = [0] * (n+1)
+dfs_result = []
+bfs_result = []
 
 dcnt = 1
 bcnt = 1
@@ -18,32 +20,28 @@ for _ in range(m):
 
 
 def dfs(r):
-    global dcnt
-    dfs_visited[r] = dcnt
-
+    dfs_visited[r] = 1
+    print(r, end=" ")
     graph[r].sort()
 
     for g in graph[r]:
         if dfs_visited[g] == 0:
-            dcnt += 1
             dfs(g)
 
 def bfs(r):
-    global bcnt
-    bfs_visited[r] = bcnt
+    bfs_visited[r] = 1
     queue.append(r)
 
     while queue:
         node = queue.popleft()
+        print(node, end=" ")
         graph[node].sort()
 
         for g in graph[node]:
             if bfs_visited[g] == 0:
-                bcnt += 1
-                bfs_visited[g] = bcnt
+                bfs_visited[g] = 1
                 queue.append(g)
 
 dfs(v)
+print()
 bfs(v)
-print(' '.join(map(str, dfs_visited[1:])))
-print(' '.join(map(str, bfs_visited[1:])))
